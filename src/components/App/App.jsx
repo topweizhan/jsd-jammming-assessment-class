8f8f4a5b788eb08cb4b4e31f381d4430f91a95c2
topweizhan <top.pirachat@gmail.com>
2022-07-20 22:27:51 +0700

    update playlist

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -13,9 +13,13 @@ class App extends Component {
     super(props)
 
     this.state = {
-      searchResults: [{name: 'name1', artist: 'artist1',album: 'album1', id: 1},
+      searchResults: [{name: 'name1', artist: 'artist1', album: 'album1', id: 1},
       {name: 'name2', artist: 'artist2', album: 'album2', id: 2}, 
-      {name: 'name3', artist: 'artist3', album: 'album3', id: 3}]
+      {name: 'name3', artist: 'artist3', album: 'album3', id: 3}],
+      playListName: 'My Playlist',
+      playListTracks: [{name: 'playlistName1', artist: 'playlistArtist1', album: 'album1', id: 4},
+      {name: 'playlistName2', artist: 'playlistArtist2', album: 'album2', id: 5},
+      {name: 'playlistName3', artist: 'playlistArtist3',album: 'album3', id: 6}]
     }
   }
 
@@ -29,7 +33,8 @@ class App extends Component {
           <SearchBar />
           <div className="App-playlist">
             <SearchResults searchResults={this.state.searchResults} />
-            <Playlist />
+            <Playlist playlistName={this.state.playListName}
+                      playlistTracks={this.state.playListTracks} />
           </div>
         </div>
       </div>
--- src/components/Playlist/Playlist.jsx
+++ src/components/Playlist/Playlist.jsx
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 
 import "./Playlist.css"
 
+import TrackList from "../TrackList/TrackList";
+
 class Playlist extends Component {
   render() {
     return (
     <div className="Playlist">
       <input defaultValue={"New Playlist"}/>
-      {/* <!-- Add a TrackList component --> */}
+      <TrackList tracks={this.props.playlistTracks} />
       <button className="Playlist-save">SAVE TO SPOTIFY</button>
     </div>)
   }
