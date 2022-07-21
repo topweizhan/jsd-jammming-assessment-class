2dde89f6af973387dd91df852bc5472fa58ae54f
topweizhan <top.pirachat@gmail.com>
2022-07-21 14:37:45 +0700

    removeTrack

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -16,11 +16,31 @@ class App extends Component {
       searchResults: [{name: 'name1', artist: 'artist1', album: 'album1', id: 1},
       {name: 'name2', artist: 'artist2', album: 'album2', id: 2}, 
       {name: 'name3', artist: 'artist3', album: 'album3', id: 3}],
-      playListName: 'My Playlist',
-      playListTracks: [{name: 'playlistName1', artist: 'playlistArtist1', album: 'album1', id: 4},
+      playlistName: 'My Playlist',
+      playlistTracks: [{name: 'playlistName1', artist: 'playlistArtist1', album: 'album1', id: 4},
       {name: 'playlistName2', artist: 'playlistArtist2', album: 'album2', id: 5},
       {name: 'playlistName3', artist: 'playlistArtist3',album: 'album3', id: 6}]
     }
+
+    this.addTrack = this.addTrack.bind(this)
+    this.removeTrack = this.removeTrack.bind(this)
+  }
+
+  addTrack(track) {
+    let tracks = this.state.playlistTracks
+    if (tracks.find(prevTrack => prevTrack.id === track.id)) {
+      return
+    }
+
+    tracks.push(track)
+    this.setState({ playlistTracks: tracks })
+  }
+
+  removeTrack(track) {
+    let tracks = this.state.playlistTracks
+    tracks = tracks.filter(currentTrack => currentTrack.id !== track.id)
+
+    this.setState({ playlistTracks: tracks })
   }
 
   render() {
@@ -32,9 +52,11 @@ class App extends Component {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            <SearchResults searchResults={this.state.searchResults} />
-            <Playlist playlistName={this.state.playListName}
-                      playlistTracks={this.state.playListTracks} />
+            <SearchResults searchResults={this.state.searchResults} 
+                          onAdd={this.addTrack} />
+            <Playlist playlistName={this.state.playlistName}
+                      playlistTracks={this.state.playlistTracks}
+                      onRemove={this.removeTrack} />
           </div>
         </div>
       </div>
